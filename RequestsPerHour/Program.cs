837c48058e97b89fb8d25efd0114f865590fd61b
M-Muzafarov <m.muzafarov@gmail.com>
2012-08-08 13:23:25 +0600

    Added tables: byPath, byIp, byBox, byFullPath

--- RequestsPerHour/Program.cs
+++ RequestsPerHour/Program.cs
@@ -5,27 +5,121 @@ using System.Text.RegularExpressions;
 
 namespace RequestsPerHour
 {
-	class Program
+	internal class Program
 	{
-		static void Main()
+
+		private readonly Regex path;
+		private readonly Regex fullpath;
+		private readonly Regex time;
+		private readonly Regex boxId; //It is a parameter of request
+		private readonly Regex ip;
+
+		public Program()
+		{
+			path = new Regex(@":27183(\/[\/\w\d]+)[\?\s]");
+			fullpath = new Regex(@":27183(\/[\/\S]+)\s");
+			time = new Regex(@"\s(\d\d:\d\d:\d\d,\d\d\d)\s");
+			boxId = new Regex(@"boxId\=([\da-f\-]+)[\@\&$\s]");
+			ip = new Regex(@"\s([\d{1,3}\.]{3}\d{1,3})\s");
+		}
+
+		private static void Main(string[] args)
+		{
+			var p = new Program();
+			p.Run();
+		}
+		private void Run()
+		{
+			ByFullPath();
+			ByIp();
+			ByPath();
+			ByBox();
+		}
+
+		private void ByBox()
 		{
 			var hash = new Dictionary<Int64, Dictionary<String, Int64>>();
-			var path = new Regex(@":27183(\/[\/\S]+)\s");
-			var time = new Regex(@"\s(\d\d:\d\d:\d\d,\d\d\d)\s");
-			//boxId=re.compile(r'boxId\=([\da-f\-]+)[\@\&$\s]')
-			//ip=re.compile(r'\s([\d{1,3}\.]{3}\d{1,3})\s')
+			var log = new StreamReader(@"../../actions_2012-08-06");
+			var boxes = new SortedSet<String>();
+			var i = 0.0;
+			Console.WriteLine("ByBox");
+			while (!log.EndOfStream)
+			{
+				Console.Write("\r{0,2:f}%", i * 100 / 6327965);
+				i++;
+				var line = log.ReadLine();
+				if (line == null) continue;
+				var bm = boxId.Match(line);
+				if (!bm.Success) continue;
+				var boxing = bm.Value.Replace("boxId=", "");
+				boxes.Add(boxing);
+				var tm = time.Match(line);
+				if (!tm.Success) continue;
+				var tim = tm.Value.Replace(',', '.').Trim();
+				var timing = Convert.ToDateTime(tim).Hour;
+				if (hash.ContainsKey(timing))
+				{
+					if (hash[timing].ContainsKey(boxing))
+					{
+						hash[timing][boxing]++;
+					}
+					else
+					{
+						hash[timing][boxing] = 1;
+					}
+				}
+				else
+				{
+					hash[timing] = new Dictionary<String, Int64>();
+					hash[timing][boxing] = 1;
+				}
+			}
+
+			var fil = new StreamWriter(@"../../Results/byBoxes.html");
+			fil.WriteLine(boxes.Count);
+			fil.Write("<table border=1 style='border-collapse: collapse;'><tr><th>BoxId</th>");
+			for (var t = 0; t < 24; t++)
+			{
+				fil.Write("<th>{0}</th>", t);
+			}
+			fil.Write("</tr>");
+			foreach (var v in boxes)
+			{
+				fil.Write("<tr>");
+				fil.Write("<th>{0}</th>", v);
+				for (var k = 0; k < 24; k++)
+				{
+					if (hash[k].ContainsKey(v))
+					{
+						fil.Write("<td>{0}</td>", hash[k][v]);
+					}
+					else
+					{
+						fil.Write("<td></td>");
+					}
+				}
+				fil.WriteLine("</tr>");
+			}
+			fil.WriteLine("</table>");
+			fil.Close();
+		}
+
+		private void ByPath()
+		{
+			var hash = new Dictionary<Int64, Dictionary<String, Int64>>();
 			var log = new StreamReader(@"../../actions_2012-08-06");
 			var paths = new SortedSet<String>();
 			var i = 0.0;
+			Console.WriteLine("ByPath");
 			while (!log.EndOfStream)
 			{
-				Console.Write("\r{0,2:f}%", i*100 / 6327965);
+				Console.Write("\r{0,2:f}%", i * 100 / 6327965);
 				i++;
 				var line = log.ReadLine();
 				if (line == null) continue;
 				var pm = path.Match(line);
 				if (!pm.Success) continue;
-				var pathing = pm.Value.Replace(":27183", "");
+				var pathing = pm.Value.Replace(":27183", "").Replace("?","");
 				paths.Add(pathing);
 				var tm = time.Match(line);
 				if (!tm.Success) continue;
@@ -49,10 +143,10 @@ namespace RequestsPerHour
 				}
 			}
 
-			var fil = new StreamWriter(@"../../Results/byFullPath.html");
+			var fil = new StreamWriter(@"../../Results/byPath.html");
 			fil.WriteLine(paths.Count);
 			fil.Write("<table border=1 style='border-collapse: collapse;'><tr><th>Path</th>");
-			for (var t = 0; t < 24;t++ )
+			for (var t = 0; t < 24; t++)
 			{
 				fil.Write("<th>{0}</th>", t);
 			}
@@ -60,12 +154,12 @@ namespace RequestsPerHour
 			foreach (var v in paths)
 			{
 				fil.Write("<tr>");
-				fil.Write("<th>{0}</th>",v);
+				fil.Write("<th>{0}</th>", v);
 				for (var k = 0; k < 24; k++)
 				{
 					if (hash[k].ContainsKey(v))
 					{
-						fil.Write("<td>{0}</td>",hash[k][v]);
+						fil.Write("<td>{0}</td>", hash[k][v]);
 					}
 					else
 					{
@@ -76,7 +170,142 @@ namespace RequestsPerHour
 			}
 			fil.WriteLine("</table>");
 			fil.Close();
+		}
 
+		private void ByIp()
+		{
+			var hash = new Dictionary<Int64, Dictionary<String, Int64>>();
+			var log = new StreamReader(@"../../actions_2012-08-06");
+			var ips = new SortedSet<String>();
+			var i = 0.0;
+			Console.WriteLine("ByIp");
+			while (!log.EndOfStream)
+			{
+				Console.Write("\r{0,2:f}%", i * 100 / 6327965);
+				i++;
+				var line = log.ReadLine();
+				if (line == null) continue;
+				var im = ip.Match(line);
+				if (!im.Success) continue;
+				var iping = im.Value.Trim();
+				ips.Add(iping);
+				var tm = time.Match(line);
+				if (!tm.Success) continue;
+				var tim = tm.Value.Replace(',', '.').Trim();
+				var timing = Convert.ToDateTime(tim).Hour;
+				if (hash.ContainsKey(timing))
+				{
+					if (hash[timing].ContainsKey(iping))
+					{
+						hash[timing][iping]++;
+					}
+					else
+					{
+						hash[timing][iping] = 1;
+					}
+				}
+				else
+				{
+					hash[timing] = new Dictionary<String, Int64>();
+					hash[timing][iping] = 1;
+				}
+			}
+
+			var fil = new StreamWriter(@"../../Results/byIp.html");
+			fil.WriteLine(ips.Count);
+			fil.Write("<table border=1 style='border-collapse: collapse;'><tr><th>Ip</th>");
+			for (var t = 0; t < 24; t++)
+			{
+				fil.Write("<th>{0}</th>", t);
+			}
+			fil.Write("</tr>");
+			foreach (var v in ips)
+			{
+				fil.Write("<tr>");
+				fil.Write("<th>{0}</th>", v);
+				for (var k = 0; k < 24; k++)
+				{
+					if (hash[k].ContainsKey(v))
+					{
+						fil.Write("<td>{0}</td>", hash[k][v]);
+					}
+					else
+					{
+						fil.Write("<td></td>");
+					}
+				}
+				fil.WriteLine("</tr>");
+			}
+			fil.WriteLine("</table>");
+			fil.Close();
+		}
+
+		private void ByFullPath()
+		{
+			var hash = new Dictionary<Int64, Dictionary<String, Int64>>();
+			var log = new StreamReader(@"../../actions_2012-08-06");
+			var fullpaths = new SortedSet<String>();
+			var i = 0.0;
+			Console.WriteLine("ByFullPath");
+			while (!log.EndOfStream)
+			{
+				Console.Write("\r{0,2:f}%", i*100/6327965);
+				i++;
+				var line = log.ReadLine();
+				if (line == null) continue;
+				var pm = fullpath.Match(line);
+				if (!pm.Success) continue;
+				var pathing = pm.Value.Replace(":27183", "");
+				fullpaths.Add(pathing);
+				var tm = time.Match(line);
+				if (!tm.Success) continue;
+				var tim = tm.Value.Replace(',', '.').Trim();
+				var timing = Convert.ToDateTime(tim).Hour;
+				if (hash.ContainsKey(timing))
+				{
+					if (hash[timing].ContainsKey(pathing))
+					{
+						hash[timing][pathing]++;
+					}
+					else
+					{
+						hash[timing][pathing] = 1;
+					}
+				}
+				else
+				{
+					hash[timing] = new Dictionary<String, Int64>();
+					hash[timing][pathing] = 1;
+				}
+			}
+
+			var fil = new StreamWriter(@"../../Results/byFullPath.html");
+			fil.WriteLine(fullpaths.Count);
+			fil.Write("<table border=1 style='border-collapse: collapse;'><tr><th>Path</th>");
+			for (var t = 0; t < 24; t++)
+			{
+				fil.Write("<th>{0}</th>", t);
+			}
+			fil.Write("</tr>");
+			foreach (var v in fullpaths)
+			{
+				fil.Write("<tr>");
+				fil.Write("<th>{0}</th>", v);
+				for (var k = 0; k < 24; k++)
+				{
+					if (hash[k].ContainsKey(v))
+					{
+						fil.Write("<td>{0}</td>", hash[k][v]);
+					}
+					else
+					{
+						fil.Write("<td></td>");
+					}
+				}
+				fil.WriteLine("</tr>");
+			}
+			fil.WriteLine("</table>");
+			fil.Close();
 		}
 	}
 }
